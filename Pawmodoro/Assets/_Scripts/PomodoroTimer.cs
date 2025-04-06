c4fef823cf9f46cf96501591a7e79b2e50fe21ec
Lindsey Yucha <yuchalin@msu.edu>
2025-04-06 19:48:52 -0400

    WIP

--- Pawmodoro/Assets/_Scripts/PomodoroTimer.cs
+++ Pawmodoro/Assets/_Scripts/PomodoroTimer.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0ee511bfb045c4c1584d0e1d34c2247682718cac4bc026b4d2e9527c180bbd71
-size 2473
+oid sha256:a6e0bc2f392c11764ef1d8c140e5b21eb650c23cffe0fed81c55b9e128e2f9d4
+size 3092
